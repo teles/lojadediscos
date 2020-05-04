a1c902d3ac7eeed60241a87eda83fdd817d5a063
Teles <jose.maciel@vivadecora.com.br>
2020-05-03 22:03:51 -0300

    #3 - volta a mostrar preco na home

--- components/Disco.js
+++ components/Disco.js
@@ -4,8 +4,8 @@ import DiscFromDetailAdapter from './DiscFromDetailAdapter'
 class Disco {
   constructor (data, type, options = {}) {
     const mapperByType = {
-      list: data => new DiscoFromListAdapter(data, options.notes),
-      detail: data => new DiscFromDetailAdapter(data, options.notes)
+      list: data => new DiscoFromListAdapter(data, options.fields),
+      detail: data => new DiscFromDetailAdapter(data)
     }
 
     const disco = mapperByType[type](data)
